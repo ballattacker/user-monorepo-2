692dc29db5b7d0b1c7aa5dd0f0fbf655e9c00839
abc <abc>
2024-10-09 14:55:06 +0700

    gitsigns line blame

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,13 +13,29 @@ return {
     end,
   },
 
-	{
-		"nvim-tree/nvim-tree.lua",
-		opts = require "configs.nvim-tree",
-	},
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = require "configs.nvim-tree",
+  },
 
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = require("configs.treesitter"),
+    "nvim-treesitter/nvim-treesitter",
+    opts = require "configs.treesitter",
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    lazy = false,
+    opts = {
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
+        delay = 0,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+        use_focus = true,
+      },
+    },
   },
 }
