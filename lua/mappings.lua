505bb2e288636cb665a36a7b51e201ebf2c04545
abc <abc>
2024-10-09 14:31:18 +0700

    binding

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,5 +1,9 @@
 require "nvchad.mappings"
 
+vim.keymap.del({ "n" }, "<leader>e")
+vim.keymap.del({ "n" }, "<tab>")
+vim.keymap.del({ "n" }, "<S-tab>")
+
 vim.keymap.set({ "n", "v", "o" }, "a", "<Nop>")
 vim.keymap.set({ "n" }, "A", "<Nop>")
 vim.keymap.set({ "n", "v", "o" }, "o", "a")
@@ -13,6 +17,22 @@ vim.keymap.set({ "n", "v" }, "<C-j>", "3j")
 vim.keymap.set({ "n", "v" }, "<C-k>", "3k")
 vim.keymap.set({ "n", "v" }, "<C-l>", "6l")
 
-vim.keymap.set({ "n" }, "eQ", ":quitall!<cr>")
-vim.keymap.set({ "n" }, "ew", ":w<cr>")
+-- these mappings kind of suck actually
 
+vim.keymap.set({ "n", "v" }, "D", '"_d')
+vim.keymap.set({ "n", "v" }, "C", '"_c')
+
+vim.keymap.set({ "n" }, "eh", function()
+  require("nvchad.tabufline").prev()
+end)
+vim.keymap.set({ "n" }, "el", function()
+  require("nvchad.tabufline").next()
+end)
+vim.keymap.set({ "n" }, "ew", ":w<cr>")
+vim.keymap.set({ "n" }, "eq", function()
+  require("nvchad.tabufline").close_buffer()
+end)
+vim.keymap.set({ "n" }, "eQ", ":quitall!<cr>")
+vim.keymap.set({ "n" }, "ex", function()
+  require("nvchad.tabufline").closeAllBufs(false)
+end)
