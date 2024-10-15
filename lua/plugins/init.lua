7b9bbc8c009497ccb3b48855746626902fe6887d
abc <abc>
2024-10-15 11:54:13 +0700

    legendary

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -168,6 +168,17 @@ local M = {
       { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
     },
   },
+
+  {
+    "mrjones2014/legendary.nvim",
+    version = "v2.13.9",
+    -- since legendary.nvim handles all your keymaps/commands,
+    -- its recommended to load legendary.nvim before other plugins
+    priority = 10000,
+    lazy = false,
+    -- sqlite is only needed if you want to use frecency sorting
+    -- dependencies = { 'kkharji/sqlite.lua' }
+  },
 }
 
 table.push(M, require "plugins.cmp")
