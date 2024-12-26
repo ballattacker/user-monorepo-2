e5a8008dd24712941282f6c05dea2ad75c066fdb
abc <abc>
2024-12-26 15:51:01 +0700

    awman f this commenting sht

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -47,6 +47,21 @@ local M = {
   },
 
   {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = false,
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        enable_autocmd = false,
+      }
+      local get_option = vim.filetype.get_option
+      vim.filetype.get_option = function(filetype, option)
+        return option == "commentstring" and require("ts_context_commentstring.internal").calculate_commentstring()
+          or get_option(filetype, option)
+      end
+    end,
+  },
+
+  {
     "lewis6991/gitsigns.nvim",
     lazy = false,
     opts = {
