254ab3529202f08d0158c4e708c875fb89e6b032
unknown <unknown>
2026-01-25 21:34:44 +0700

    ft plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -34,48 +34,6 @@ local M = {
   },
 
   {
-    "RaafatTurki/hex.nvim",
-    config = function(_, _)
-      require("hex").setup()
-
-      vim.keymap.set({ "n" }, "<leader>hxi", function()
-        require("hex").dump()
-      end, { desc = "Edit in hex on" })
-      vim.keymap.set({ "n" }, "<leader>hxo", function()
-        require("hex").assemble()
-      end, { desc = "Edit in hex off" })
-    end,
-  },
-
-  {
-    "hat0uma/csvview.nvim",
-    ---@module "csvview"
-    ---@type CsvView.Options
-    opts = {
-      parser = { comments = { "#", "//" } },
-      keymaps = {
-        -- Text objects for selecting fields
-        textobject_field_inner = { "if", mode = { "o", "x" } },
-        textobject_field_outer = { "af", mode = { "o", "x" } },
-        -- Excel-like navigation:
-        -- Use <Tab> and <S-Tab> to move horizontally between fields.
-        -- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
-        -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
-        jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
-        jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
-        jump_next_row = { "<Enter>", mode = { "n", "v" } },
-        jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
-      },
-    },
-    cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
-  },
-
-  {
-    "gpanders/nvim-parinfer",
-    ft = { "lisp", "fennel" },
-  },
-
-  {
     "stevearc/oil.nvim",
     ---@module 'oil'
     ---@type oil.SetupOpts
@@ -102,5 +60,6 @@ table.push(M, require "plugins.git")
 table.push(M, require "plugins.lsp")
 table.push(M, require "plugins.cmp")
 table.push(M, require "plugins.syntax")
+table.push(M, require "plugins.ft")
 
 return M
