1a5720c663097d92d744512796606ed135eeff65
unknown <unknown>
2026-02-04 01:17:12 +0700

    add lazydev.nvim to fix the annoying undefined type in lua config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,8 +1,20 @@
 local M = {
   {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+
+  {
     "klen/nvim-config-local",
     lazy = false,
-    -- priority = 60,
+    -- priority = 20,
     config = function()
       vim.g.config_local = {}
       require("config-local").setup {
