70ad6804f3140c970e89fd75ad139ca943b4ee20
unknown <unknown>
2026-02-04 23:12:18 +0700

    bye legendary you will be missed

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -28,17 +28,6 @@ local M = {
       }
     end,
   },
-
-  {
-    "mrjones2014/legendary.nvim",
-    version = "v2.13.9",
-    -- since legendary.nvim handles all your keymaps/commands,
-    -- its recommended to load legendary.nvim before other plugins
-    priority = 10000,
-    lazy = false,
-    -- sqlite is only needed if you want to use frecency sorting
-    -- dependencies = { 'kkharji/sqlite.lua' }
-  },
 }
 
 return M
