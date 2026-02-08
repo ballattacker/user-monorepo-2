04a13607c932bca5f9b1f5aa0b1d417387347761
unknown <unknown>
2026-02-08 15:55:08 +0700

    clean ip

--- lua/plugins/nav.lua
+++ lua/plugins/nav.lua
@@ -70,38 +70,6 @@ local M = {
         end,
         remap = true,
       },
-      {
-        "sg",
-        function()
-          local hop = require "hop"
-          hop.hint_with(function(_opts)
-            -- local jump_targets = {
-            --   { window = 0, buffer = 0, cursor = { row = 1, col = 14 }, length = 1 },
-            --   { window = 0, buffer = 0, cursor = { row = 2, col = 1 }, length = 1 },
-            -- }
-            local jump_targets = {}
-            local min_x_distance = 10
-            -- local min_y_distance = 5
-            for i = 1, vim.api.nvim_buf_line_count(0) do
-              local line = vim.api.nvim_buf_get_lines(0, i - 1, i, false)[1]
-              local start_col = line:find "%S" or 1
-              local end_col = #line
-              for j = start_col, end_col, min_x_distance do
-                table.insert(jump_targets, {
-                  window = 0,
-                  buffer = 0,
-                  cursor = { row = i, col = j - 1 },
-                  length = 1,
-                })
-              end
-            end
-            return {
-              jump_targets = jump_targets,
-            }
-          end, hop.opts)
-        end,
-        remap = true,
-      },
     },
   },
 }
