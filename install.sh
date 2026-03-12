1f21a44c89e78ba1a056e88334a83930ebec7ba2
unknown <unknown>
2026-03-12 17:35:29 +0700

    for usrp2

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+cd "$(dirname "$0")" || exit
+name=neovim
+usrp install mise
+nix profile add nixpkgs#$name
+ln -frs . "$XDG_CONFIG_DIR"/nvim
+ln -frs ./env "$POSIX_DIR"/nvim.sh
+
+deps="
+  unzip
+  gcc
+"
+
+for dep in $deps; do
+  nix profile add nixpkgs#"$dep"
+done
+
+deps="
+  bun
+  deno
+  go
+  node
+  python
+  rust
+  zig
+"
+
+for dep in $deps; do
+  mise use --global "$dep"@latest
+done
