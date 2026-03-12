9dc76a2a1dafeff1c5f02ebe9fdb2e404e5e7583
unknown <unknown>
2026-03-13 00:07:03 +0700

    trying to fix preview

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ln -fns ./preview.sh "$HOME/.local/bin/preview.sh"
-ln -fns ./preview-less.sh "$HOME/.local/bin/preview-less.sh"
+ln -fnrs ./preview.sh "$HOME/.local/bin/preview.sh"
+ln -fnrs ./preview-less.sh "$HOME/.local/bin/preview-less.sh"
