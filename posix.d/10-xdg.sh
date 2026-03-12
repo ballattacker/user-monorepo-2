ae06367639303d0a09a3edcd83ad929e0a646d39
unknown <unknown>
2026-03-12 15:03:01 +0700

    local bin

--- posix.d/10-xdg.sh
+++ posix.d/10-xdg.sh
@@ -7,3 +7,4 @@ export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
 
 mkdir -p "$XDG_CONFIG_HOME" "$XDG_DATA_HOME" "$XDG_CACHE_HOME" "$XDG_STATE_HOME"
+mkdir -p "$HOME/.local/bin"
