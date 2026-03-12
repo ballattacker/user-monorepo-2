e8b1eb0ff9d3b522b964a8c45e6c129c9d728fb0
unknown <unknown>
2026-03-12 17:13:10 +0700

    .sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ name=yazi
 usrp install preview
 nix profile add nixpkgs#$name
 ln -frs . "$XDG_CONFIG_DIR"/$name
-ln -frs ./hook "$POSIX_DIR"/$name
+ln -frs ./hook "$POSIX_DIR"/"$name".sh
 
 deps="
 	fzf
