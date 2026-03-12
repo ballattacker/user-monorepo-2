67cf4d466095bcd657ba34515ca5fe5cec3e0885
unknown <unknown>
2026-03-12 17:15:47 +0700

    fix bin

--- xdg.sh
+++ xdg.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+export XDG_BIN_HOME="$HOME/.local/bin"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
 
-mkdir -p "$XDG_CONFIG_HOME" "$XDG_DATA_HOME" "$XDG_CACHE_HOME" "$XDG_STATE_HOME"
-mkdir -p "$HOME/.local/bin"
+mkdir -p "$XDG_BIN_HOME" "$XDG_CONFIG_HOME" "$XDG_DATA_HOME" "$XDG_CACHE_HOME" "$XDG_STATE_HOME"
+export PATH="$XDG_BIN_HOME:$PATH"
