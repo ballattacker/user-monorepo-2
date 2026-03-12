3e64486ff7d3933347fef36a2e0eb1291c1692f4
unknown <unknown>
2026-03-12 20:01:47 +0700

    no need to chmod

--- install.sh
+++ install.sh
@@ -2,6 +2,3 @@
 
 ln -fns ./preview.sh "$HOME/.local/bin/preview.sh"
 ln -fns ./preview-less.sh "$HOME/.local/bin/preview-less.sh"
-
-chmod +x "$HOME/.local/bin/preview.sh"
-chmod +x "$HOME/.local/bin/preview-less.sh"
