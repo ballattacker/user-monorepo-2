b485dbcf302b5257308ddb2a4d1247be6fc28ef9
unknown <unknown>
2026-03-12 19:30:29 +0700

    fix XDG_CONFIG_HOME

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ cd "$(dirname "$0")" || exit
 name=default
 usrp install dep
 nix profile add nixpkgs#$name
-ln -frs . "$XDG_CONFIG_DIR"/$name
+ln -frs . "$XDG_CONFIG_HOME"/$name
 ln -frs ./env "$POSIX_DIR"/"$name".sh
 ln -frs ./bin "$HOME"/.local/bin/$name
