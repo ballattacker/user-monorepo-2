1c83856d0484b70b5453b4e8d84a49a7029273bf
unknown <unknown>
2026-03-13 00:09:11 +0700

    preview deps

--- install.sh
+++ install.sh
@@ -2,3 +2,13 @@
 
 ln -fnrs ./preview.sh "$HOME/.local/bin/preview.sh"
 ln -fnrs ./preview-less.sh "$HOME/.local/bin/preview-less.sh"
+
+deps="
+  atool
+  bat
+  chafa
+"
+
+for dep in $deps; do
+  nix profile add nixpkgs#"$dep"
+done
