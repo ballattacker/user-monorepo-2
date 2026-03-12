1e178404b846c0aa3485c101331a0719f98acfad
unknown <unknown>
2026-03-12 19:13:39 +0700

    fix setup home

--- manage.sh
+++ manage.sh
@@ -77,7 +77,7 @@ setup)
   nix registry add nixpkgs github:numtide/nixpkgs-unfree/nixos-unstable
 
   mkdir -p "$repo_dir/posix.d"
-  echo "eval \$($repo_dir/activate)" >>"${1:-~/.bashrc}"
+  echo "eval \$($repo_dir/activate)" >>"${1:-$HOME/.bashrc}"
 
   mkdir -p "$HOME"/.local/bin
   ln -frs "$repo_dir"/manage.sh "$HOME"/.local/bin/usrp
