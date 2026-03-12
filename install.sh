58b4736188091367c975cc37dbab4591733851e0
unknown <unknown>
2026-03-12 17:11:33 +0700

    template .sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ name=default
 usrp install dep
 nix profile add nixpkgs#$name
 ln -frs . "$XDG_CONFIG_DIR"/$name
-ln -frs ./env "$POSIX_DIR"/$name
+ln -frs ./env "$POSIX_DIR"/"$name".sh
 ln -frs ./bin "$HOME"/.local/bin/$name
