ea7b871cd12e585e78f0281da135eb03bbb15b39
unknown <unknown>
2026-03-13 19:33:34 +0700

    module/dev init

--- install.sh
+++ install.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
-name=default
-usrp install dep
+name=dev
+usrp install mise
+eval "$("$POSIX_DIR"/../activate)"
 nix profile add nixpkgs#$name
-ln -fnrs . "$XDG_CONFIG_HOME"/$name
 ln -fnrs ./env "$POSIX_DIR"/"$name".sh
-ln -fnrs ./bin "$HOME"/.local/bin/$name
+ln -fnrs ./python "$XDG_CONFIG_HOME"/python
+
+deps="
+  bun
+  deno
+  go
+  python
+  rust
+  zig
+"
+
+for dep in $deps; do
+  mise use --global "$dep"@latest
+done
+mise use --global node@24
