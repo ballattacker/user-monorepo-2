ebdf488d10447b072cf00720eed59ef76cba820e
default <default>
2026-03-15 00:09:34 +0700

    pin versions

--- install.sh
+++ install.sh
@@ -12,5 +12,8 @@ nix profile add nixpkgs#starship
 git clone https://github.com/Aloxaf/fzf-tab plugins/fzf-tab
 git -C plugins/fzf-tab checkout 5a81e13
 git clone https://github.com/zsh-users/zsh-autosuggestions plugins/zsh-autosuggestions
+git -C plugins/zsh-autosuggestions checkout 85919cd
 git clone https://github.com/zsh-users/zsh-history-substring-search plugins/zsh-history-substring-search
+git -C plugins/zsh-history-substring-search checkout 14c8d2e
 git clone https://github.com/zsh-users/zsh-syntax-highlighting plugins/zsh-syntax-highlighting
+git -C plugins/zsh-syntax-highlighting checkout 1d85c69
