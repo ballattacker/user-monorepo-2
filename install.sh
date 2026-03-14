bd53a6bc35184864d872af295d24aa3356e2f1d4
default <default>
2026-03-14 22:22:51 +0700

    add path

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ nix profile add nixpkgs#$name
 ln -fnrs . "$XDG_CONFIG_HOME"/$name
 ln -fnrs ./env "$POSIX_DIR"/"$name".sh
 ln -fnrs ./bin "$HOME"/.local/bin/$name
+dir="$(cd "$(dirname "$0")" && pwd)"
+echo "export PATH=\"$dir\${PATH:+\":\$PATH\"}\"" >"$POSIX_DIR"/"$name".sh
