730bce494357a0e7241a76b0c7deebbe4754a686
default <default>
2026-03-14 22:40:50 +0700

    use printf

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ ln -fnrs . "$XDG_CONFIG_HOME"/$name
 ln -fnrs ./env "$POSIX_DIR"/"$name".sh
 ln -fnrs ./bin "$HOME"/.local/bin/$name
 dir="$(cd "$(dirname "$0")" && pwd)"
-echo "export PATH=\"$dir\${PATH:+\":\$PATH\"}\"" >"$POSIX_DIR"/"$name".sh
+printf 'export PATH="%s${PATH:+":$PATH"}"' "$dir" >"$POSIX_DIR"/"$name".sh
