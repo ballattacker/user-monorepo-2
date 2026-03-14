9dfbbf99b132d6eddc5404c74133a3b95efe4637
default <default>
2026-03-14 21:29:16 +0700

    path

--- xdg.sh
+++ xdg.sh
@@ -8,4 +8,4 @@ export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
 
 mkdir -p "$XDG_BIN_HOME" "$XDG_CONFIG_HOME" "$XDG_DATA_HOME" "$XDG_CACHE_HOME" "$XDG_STATE_HOME"
-export PATH="$XDG_BIN_HOME:$PATH"
+export PATH="$XDG_BIN_HOME${PATH:+":$PATH"}"
