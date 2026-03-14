419606e7adc27db0ff7a16f65713b9f6f70777a4
default <default>
2026-03-14 21:37:16 +0700

    move to using path

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ln -fnrs ./preview.sh "$HOME/.local/bin/preview.sh"
-ln -fnrs ./preview-less.sh "$HOME/.local/bin/preview-less.sh"
+dir="$(cd "$(dirname "$0")" && pwd)"
+export PATH="$dir${PATH:+":$PATH"}"
 
 deps="
   atool
