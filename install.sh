7b6e5b88c4db6badea01dbca0ebae31f35b646f0
default <default>
2026-03-15 01:48:28 +0700

    module/wsl-xclip init

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-name=default
-usrp install dep
-nix profile add nixpkgs#$name
-ln -fnrs . "$XDG_CONFIG_HOME"/$name
-ln -fnrs ./env "$POSIX_DIR"/"$name".sh
-ln -fnrs ./bin "$HOME"/.local/bin/$name
-dir="$(cd "$(dirname "$0")" && pwd)"
-printf 'export PATH="%s${PATH:+":$PATH"}"' "$dir" >"$POSIX_DIR"/"$name".sh
+name=wsl-xclip
+nix profile add nixpkgs#xclip
+ln -fnrs ./xclip "$HOME"/.local/bin/xclip
+# dir="$(cd "$(dirname "$0")" && pwd)"
+# printf 'export PATH="%s${PATH:+":$PATH"}"' "$dir" >"$POSIX_DIR"/"$name".sh
