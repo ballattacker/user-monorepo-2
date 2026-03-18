f3875dd6ee18da18ff1936d8826128124089b511
default <default>
2026-03-18 19:09:43 +0700

    starship

--- init.lua
+++ init.lua
@@ -1,12 +1,16 @@
-require("session"):setup {
-  sync_yanked = true,
-}
+require("session"):setup({
+	sync_yanked = true,
+})
 
-require("zoxide"):setup {
-  update_db = true,
-}
+require("zoxide"):setup({
+	update_db = true,
+})
 
-require("git"):setup {
+require("git"):setup({
 	-- Order of status signs showing in the linemode
 	order = 1500,
-}
+})
+
+require("starship"):setup({
+	show_right_prompt = true,
+})
