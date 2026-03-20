6fcb4ec3c730da7c1c36b7fa2415c5edfbb78d69
default <default>
2026-03-20 10:57:06 +0700

    module/nix init

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-name=default
-usrp install dep
-nix profile add nixpkgs#$name
-ln -fnrs . "$XDG_CONFIG_HOME"/$name
-ln -fnrs ./env "$POSIX_DIR"/"$name".sh
-ln -fnrs ./bin "$HOME"/.local/bin/$name
-dir="$(cd "$(dirname "$0")" && pwd)"
-printf 'export PATH="%s${PATH:+":$PATH"}"' "$dir" >"$POSIX_DIR"/"$name".sh
+name=nix
+nix profile add nixpkgs#nix-search-cli
+nix profile add nixpkgs#nix-index
